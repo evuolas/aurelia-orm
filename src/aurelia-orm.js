bda89d0e6a6aa55cbe18be3d24c93254e936aa76
Jere Piispanen <jere.piispanen@gmail.com>
2016-12-22 12:09:35 +0200

    fix(build): fix build

--- src/aurelia-orm.js
+++ src/aurelia-orm.js
@@ -31,3 +31,19 @@ export function configure(frameworkConfig, configCallback) {
 }
 
 export const logger = getLogger('aurelia-orm');
+
+export {DefaultRepository} from './default-repository';
+export {Repository} from './repository';
+export {OrmMetadata} from './orm-metadata';
+
+export {association} from './decorator/association';
+export {endpoint} from './decorator/endpoint';
+export {jsonRoot} from './decorator/json-root';
+export {name} from './decorator/name';
+export {repository} from './decorator/repository';
+export {resource} from './decorator/resource';
+export {type} from './decorator/type';
+export {validatedResource} from './decorator/validated-resource';
+export {validation} from './decorator/validation';
+
+export {Entity, EntityManager, ValidationRules};
